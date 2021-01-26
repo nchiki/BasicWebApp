97360ce7eb120c792d1fd671610860fa889dc903
Nahida Chiki <nahidachiki@Nahidas-MBP.fritz.box>
2021-01-26 12:08:24 +0100

    added new query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        return "NOT FOUND";
+        if (query.toLowerCase().contains("turing")) {
+            return "Turing was highly influential in the development of theoretical computer science, " +
+                    "providing a formalisation of the concepts of algorithm and computation with the Turing machine, " +
+                    "which can be considered a model of a general-purpose computer. ";
+        }
+        return "";
     }
 }
