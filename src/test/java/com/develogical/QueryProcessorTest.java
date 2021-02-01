e268581f6f8a23498540daa1733417fc4dac31ca
Nahida Chiki <nahidachiki@Nahidas-MBP.fritz.box>
2021-02-01 19:13:21 +0100

    dummy commit

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -1,7 +1,6 @@
 package com.develogical;
 
 import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
